b2d44f563dd0aedb82f063d5e8dba369e0f7f3c7
Mark Kelsall <99473523+geekissh@users.noreply.github.com>
2024-01-09 09:50:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email,
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formData),
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        
+        document.body.innerHTML += `<p>User ID: ${userId}</p>`;
+      })
+      .catch(function (error) {
+       
+        document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+      });
+  }
+  
+  submitData("John Doe", "john.doe@example.com");
